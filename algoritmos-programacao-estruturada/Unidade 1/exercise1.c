963fae53e56ce8a5cf6cedb8d2bee5a7e509966f
Malu Rezende <maluar@gmail.com>
2022-01-15 14:25:40 -0300

    feat: create new logic and struct

--- algoritmos-programacao-estruturada/Unidade 1/exercise1.c
+++ algoritmos-programacao-estruturada/Unidade 1/exercise1.c
@@ -2,36 +2,52 @@
 
 struct dadoscliente
 {
-    char nomecliente[50], nomefilme[50];
-    int idadecliente, classifilme;
+    char nomecliente[50]; 
+    int idadecliente, tempolocacao; 
 };
 
+struct dadosfilme
+{
+    char nomefilme[50];
+    int classifilme;
+};
+
+
 int main() {
-    struct dadoscliente infocliente;
+    struct dadoscliente cliente; 
+    struct dadosfilme filme; 
+    
+
     printf("Ficha de cadastro dos Clientes");
      
     printf("\nNome do cliente: ");
-    scanf("%s", infocliente.nomecliente);
+    scanf("%s", cliente.nomecliente);
     
     printf("\nIdade: ");
-    scanf("%d", &infocliente.idadecliente);
+    scanf("%d", &cliente.idadecliente);
     
     printf("\nNome do filme:");
-    scanf("%s", infocliente.nomefilme); 
+    scanf("%s", filme.nomefilme); 
     
     printf("\nClassificação indicada para o filme: ");
-    scanf("%d", &infocliente.classifilme); 
+    scanf("%d", &filme.classifilme); 
 
-    printf("\nCliente: %s\n", infocliente.nomecliente);
-    printf("Idade: %d anos\n", infocliente.idadecliente);
-    printf("Classificação do filme: %d anos\n", infocliente.classifilme);
+    printf("\nCliente: %s\n", cliente.nomecliente);
+    printf("Idade: %d anos\n", cliente.idadecliente);
+    printf("Classificação do filme: %d anos\n", filme.classifilme);
 
     
-    if (infocliente.idadecliente <= infocliente.classifilme) {
-    printf("\n%s, \nEste filme não é indicado para sua faixa etária. Escolha outro!\n", infocliente.nomecliente);
+    if (cliente.idadecliente <= filme.classifilme) {
+    printf("\n%s, \nEste filme não é indicado para sua faixa etária. Escolha outro!\n", cliente.nomecliente);
     } else {
         printf("\nFilme alugado com sucesso!\n");
     }; 
+
+    cliente.tempolocacao = filme.classifilme - cliente.idadecliente;
+
+    printf("\nFaltam %d anos para que você possa alugar este filme.\n", cliente.tempolocacao);
+    
+    
 };
 
 
